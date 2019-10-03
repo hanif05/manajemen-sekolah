d253fb27ae20c87793365d98a704d2079a1de66b
hanif05 <muamarhanif05@gmail.com>
2019-10-03 16:34:16 +0700

    make Kelas Seeder

---
+++ database/seeds/KelasTableSeeder.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class KelasTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        if(DB::table('kelas')->get()->count() == 0){
+
+            DB::table('kelas')->insert([
+
+                [
+                    'nama_kelas' => '7-A',
+                ],
+                [
+                    'nama_kelas' => '7-B',
+                ],
+                [
+                    'nama_kelas' => '7-C',
+                ],
+                [
+                    'nama_kelas' => '8-A',
+                ],
+                [
+                    'nama_kelas' => '8-B',
+                ],
+                [
+                    'nama_kelas' => '8-C',
+                ],
+                [
+                    'nama_kelas' => '9-A',
+                ],
+                [
+                    'nama_kelas' => '9-B',
+                ],
+                [
+                    'nama_kelas' => '9-C',
+                ],
+
+
+            ]);
+
+        } else { echo "Table is not empty"; }
+    }
+}
